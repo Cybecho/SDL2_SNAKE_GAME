1987b33141bce388076349e1640ad9ea766fe40f
Cybecho_Proxy <42949995+Cybecho@users.noreply.github.com>
2024-05-21 20:49:08 +0900

    아이템 객체 추가

--- src/obj_Item.h
+++ src/obj_Item.h
@@ -6,7 +6,6 @@ class Item : public Obj {
 public:
     Item(int x, int y, SDL_Renderer* renderer);
     virtual ~Item();
-    void update();
     void render(SDL_Renderer* renderer) const;
 
     //! 현재 객체 타입 반환
@@ -14,4 +13,27 @@ public:
 
 protected:
     static SDL_Texture* mTexture;
+};
+
+class trap_Item : public Obj {
+public:
+    trap_Item(int x, int y, SDL_Renderer* renderer);
+    virtual ~trap_Item();
+    void render(SDL_Renderer* renderer) const;
+
+    //! 현재 객체 타입 반환
+    int getType() const { return mType; }
+
+protected:
+    static SDL_Texture* mTexture;
+};
+
+class cheat_Item : public Obj {
+public:
+	cheat_Item(int x, int y, SDL_Renderer* renderer);
+	virtual ~cheat_Item();
+	void render(SDL_Renderer* renderer) const;
+
+	//! 현재 객체 타입 반환
+    int getType() const { return mType; }
 };
